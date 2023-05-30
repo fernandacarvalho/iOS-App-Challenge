826dec49346c8a203cb9269eb4034cfdfea26cc0
Fernanda Carvalho <fernanda.carvalho@mobicare.com.br>
2023-05-30 00:13:26 -0300

    Util class for image download

---
+++ Books/Books/Utils/ImageProvider.swift
@@ -0,0 +1,72 @@
+//
+//  ImageProvider.swift
+//  Books
+//
+//  Created by Fernanda FC. Carvalho on 29/05/23.
+//
+
+import Foundation
+
+final class ImageProvider {
+    static func download(url: URL, toFile file: URL, completion: @escaping (Error?) -> Void) {
+        // Download the remote URL to a file
+        let task = URLSession.shared.downloadTask(with: url) {
+            (tempURL, response, error) in
+            // Early exit on error
+            guard let tempURL = tempURL else {
+                completion(error)
+                return
+            }
+
+            do {
+                // Remove any existing document at file
+                if FileManager.default.fileExists(atPath: file.path) {
+                    try FileManager.default.removeItem(at: file)
+                }
+
+                // Copy the tempURL to file
+                try FileManager.default.copyItem(
+                    at: tempURL,
+                    to: file
+                )
+
+                completion(nil)
+            }
+
+            // Handle potential file system errors
+            catch let fileError {
+                completion(fileError)
+            }
+        }
+
+        // Start the download
+        task.resume()
+    }
+    
+    static func loadData(url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        // Compute a path to the URL in the cache
+        let fileCachePath = FileManager.default.temporaryDirectory
+            .appendingPathComponent(
+                url.lastPathComponent,
+                isDirectory: false
+            )
+        
+        // If the image exists in the cache,
+        // load the image from the cache and exit
+        if let data = try? Data(contentsOf: fileCachePath) {
+            completion(data, nil)
+            return
+        }
+        
+        // If the image does not exist in the cache,
+        // download the image to the cache
+        download(url: url, toFile: fileCachePath) { (error) in
+            if let data = try? Data(contentsOf: fileCachePath) {
+                completion(data, error)
+                return
+            } else {
+                completion(nil, error)
+            }
+        }
+    }
+}
